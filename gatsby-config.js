43d68dd4a97b50376c2bc274f8f5673f86446161
Rishab Kumar <rishabkumar7@gmail.com>
2020-07-11 10:56:54 -0400

    Changed meta data

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,9 +48,9 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
+        name: `Rishab's minimal blog`,
         short_name: `minimal-blog`,
-        description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
+        description: `My Cloud journey and great tutorials for AWS and Linux`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#6B46C1`,
